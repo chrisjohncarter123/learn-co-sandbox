c4b4c5557f2980a6493725ac771d5f140f8afa3f
Christopher Carter <chrisjohncarter123@gmail.com>
2020-07-17 18:40:58 +0000

    Automatically backed up by Learn

--- wisdommine2/lib/wisdommine2.rb
+++
@@ -1,6 +0,0 @@
-require "wisdommine2/version"
-
-module Wisdommine2
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- wisdommine2/lib/wisdommine2/version.rb
+++
@@ -1,3 +0,0 @@
-module Wisdommine2
-  VERSION = "0.1.0"
-end
